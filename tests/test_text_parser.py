d61f16c80ac6505ec30934423000d528cfab812c
angelomp2001 <apassala@gmail.com>
2026-05-28 21:24:23 -0500

    debug/ commented out test_text_parser.py/pytest.mar.dependency

--- tests/test_text_parser.py
+++ tests/test_text_parser.py
@@ -3,21 +3,28 @@ import pandas as pd
 import pytest
 from src.generate_sample_df import generate_sample_df
 from src.llm.text_parser import llm_parses_to_ops
+from src.llm.operations import SUPPORTED_OPS
+
 # Add tests for llm_parses_to_ops here in the future
-@pytest.mark.dependency(
-    depends=["test_call_llm_for_json", "test_apply_operation_fillna"],
-    scope="session"
-)
+# @pytest.mark.dependency(
+#     depends=["test_call_llm_for_json", "test_apply_operation_fillna"],
+#     scope="session"
+# )
 def test_llm_parses_to_ops():
     df = generate_sample_df(n_rows=10)
     # Introduce missing data to make the user text logical
-    df.loc[0, "Survived"] = np.nan
+    df.loc[0, "uniform"] = np.nan
     
     ops = llm_parses_to_ops(
-        user_text="fill missing Survived with median", 
+        user_text="fill missing uniform with median", 
         df=df,
         temperature=0.0
         )
+
+    print("OPS:", ops)
+    print("SUPPORTED_OPS:", SUPPORTED_OPS)
+
+
     assert isinstance(ops, list)
     assert len(ops) > 0
     assert ops[0]["op"] == "fillna"
